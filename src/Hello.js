3845e286e23f97ba4a8053a0f3465a5e2c8d63a9
Catie Hart <cmh@anniecannons.com>
2021-02-19 11:42:20 -0800

    First commit

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import Response from './Response'
+
+const Hello = () => {
+    return (
+        <div>
+            <p>Hi! How are you?</p>
+            <Response />
+        </div>
+    )
+}
+
+export default Hello;
---
+++ src/Response.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Response = () => {
+    return (
+        <div>I am doing great, thank you very much!</div>
+    )
+    
+}
+
+export default Response;
